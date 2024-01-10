25a25f31a3169879163eff58697ec46c0223877d
Justin Prim <justinprim140969@gmail.com>
2024-01-10 12:25:02 +0200

    Add project files.

---
+++ 1. Hello world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            //Freze console (comment to remember stop of application)
+            Console.ReadLine();
+        }
+    }
+}
